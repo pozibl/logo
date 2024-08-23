8318ae46829b42e82e6caa05dbbe1994ccf16585
leigh <leigh.maidment@pozibl.com>
2024-08-23 10:59:06 +0800

    create black background logo

--- logo.py
+++ logo.py
@@ -17,6 +17,7 @@ So start by making a few loops and adjusting till we get something similar
 from shapely.geometry import LineString, Polygon
 import matplotlib.pyplot as plt
 import geopandas as gpd
+from shapely import box
 
 
 def main():
@@ -37,10 +38,18 @@ def main():
         ls = gpd.GeoSeries(l)
 
         fig = ls.plot(color='black')
-        ax = fig.set_xlim(0, 100)
-        ax = fig.set_ylim(0, 100)
+        ax = fig.set_xlim(-50, 150)
+        ax = fig.set_ylim(-50, 150)
         plt.axis('off')
-        plt.savefig(f"logo_{i:>02}.jpg", transparent=True)
+        plt.savefig(f"logo_white{i:>02}.jpg", transparent=True)
+
+        ax = ls.plot(color='white')
+        xMin, yMin, xMax, yMax = ls.total_bounds
+        bbox = gpd.GeoDataFrame(geometry=[box(xMin-50, yMin-50, xMax+50, yMax+50)], crs=ls.crs)
+        fig = bbox.plot(ax=ax, color="black", zorder=0)
+        fig.set_facecolor('red')
+        plt.axis('off')
+        plt.savefig(f"logo_black{i:>02}.jpg", transparent=False)
 
 
 
